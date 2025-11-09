22665aad87f4a801e55ce2472611da0d634c10a3
Seafoamm <brancel8@gmail.com>
2025-11-09 14:46:08 -0800

    change deploy branch

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/bbnme/',
   plugins: [svelte()],
   server: {
     port: 2025,
